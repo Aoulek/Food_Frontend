9ee12e458bd9e4c9106bc1ec0f1c840223881c01
unknown <ibtissam.aoulek@etu.uae.ac.ma>
2022-11-26 01:48:24 +0100

    add project

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthDataService } from '../services/auth-data.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  public form={
+    name:null,
+    email:null,
+    password:null,
+    Cpassword:null
+  }
+  public error:any=[];
+  constructor(private authDataService:AuthDataService) { }
+
+  ngOnInit(): void {
+  }
+
+  submitSignup(){
+    //console.log(this.form);
+    return this.authDataService.signup(this.form).subscribe(
+      //data=>console.log(data),
+      error=>this.handleError(error)
+    );
+  }
+
+  handleError(error:any){
+    this.error=error.error.errors
+  }
+}
